8dd04c5000b44ca5795d5e74855dc5e48a84d796
Chris Miller (Chrxs) <pirate.chrxs@gmail.com>
2024-01-02 13:55:29 +0000

    Set up DB

---
+++ src/app/users/page.tsx
@@ -0,0 +1,17 @@
+import prisma from "@/utils/prisma";
+
+
+export default async function UsersPage() {
+  const users = await prisma.user.findMany();
+
+  return (
+    <main>
+      <h1 className="font-bold">Users</h1>
+      <ul>
+        {users.map((user) => (
+          <li key={user.id}>{user.name}</li>
+        ))}
+      </ul>
+    </main>
+  );
+}
