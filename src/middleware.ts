3d05685284cd20fae7bc74e10ffef7bf707c73e8
Chris Miller (Chrxs) <pirate.chrxs@gmail.com>
2024-01-04 13:13:04 +0000

    update middleware

--- src/middleware.ts
+++ src/middleware.ts
@@ -2,4 +2,17 @@ import { auth } from "@/auth";
 
 export const middleware = auth;
 
-export const config = { matcher: ["/users", "/dashboard"] };
+export const config = {
+  matcher: [
+    /*
+     * Match all request paths except for the ones starting with:
+     * - api (API routes)
+     * - _next/static (static files)
+     * - _next/image (image optimization files)
+     * - favicon.ico (favicon file)
+     */
+    "/((?!api|_next/static|_next/image|favicon.ico).*)",
+    // "/users",
+    // "/dashboard"
+  ],
+};
